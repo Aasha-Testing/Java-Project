cb7580345495b13ade4fbabaace698d1bc96953d
Aasha-Testing <durgasri00@gmail.com>
2021-06-16 14:36:55 +0530

    Üpdated code new

--- src/org/test/GitSample.java
+++ src/org/test/GitSample.java
@@ -3,6 +3,12 @@ package org.test;
 public class GitSample {
 	public static void main(String[] args) {
 		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(8);
+		System.out.println(9);
 	}
 
 }
